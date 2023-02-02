9e4b7906545cfa3ff7addec8c1def71b40cb14ec
Larissa Sena <lai.developer@hotmail.com>
2023-02-02 00:13:06 -0300

    atividade 3

--- Atividades-lista/Atividade-3/src/Main.java
+++ Atividades-lista/Atividade-3/src/Main.java
@@ -3,11 +3,16 @@ public class Main {
     TODO array e retorne o valor da soma. Ex: [1,2,3] -> 6.*/
     public static void main(String[] args) {
         int[] inteiros={2,4,7};
-        sum(inteiros);
+        int resultado=sum(inteiros);
+        System.out.println(resultado);
     }
-    public static void sum(int[] array){
+    public static int sum(int[] array){
+        int soma=0;
+
         for (int i=0;i<array.length;i++){
-            System.out.println(i);
+            System.out.println(array[i]);
+            soma = soma + array[i];
         }
+        return soma;
     }
 }
